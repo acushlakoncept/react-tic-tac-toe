fcfa8925f92a0715508627aaa84d5042e7a24647
Uduak Essien <acushla4real@gmail.com>
2020-11-04 14:05:20 +0100

    update index.js with render element

--- src/components/board.js
+++ src/components/board.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Square } from "./square";
 
-class Board extends React.Component {
+export class Board extends React.Component {
   renderSquare(i) {
     return <Square value={i} />;
   }
@@ -31,5 +31,3 @@ class Board extends React.Component {
     );
   }
 }
-
-export default Board;
--- src/components/square.js
+++ src/components/square.js
@@ -1,13 +1,7 @@
-import React from 'react'
+import React from "react";
 
-class Square extends React.Component {
-    render() {
-        return (
-            <button className="square">
-                {this.props.value}
-            </button>
-        );
-    }
+export class Square extends React.Component {
+  render() {
+    return <button className="square">{this.props.value}</button>;
+  }
 }
-
-export default Square;
